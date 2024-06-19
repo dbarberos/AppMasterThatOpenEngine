d615388758871fdf780ae940e76aad2fa2c951ba
dbarberos <dbarberos@hotmail.com>
2024-06-19 13:05:36 +0200

    createElement in the DOM using JS

--- src/classes/Project.ts
+++ src/classes/Project.ts
@@ -13,11 +13,17 @@ export interface IProject {
 
 
 export class Project implements IProject {
+    // To satisfy IProject
     name: string
     description: string
     status: "Pending" | "Active" | "Finished"
     userRole: "Architect" | "Engineer" | "Developer"
-    finishDate : Date
+    finishDate: Date
+ 
+    // Class internals
+    ui: HTMLDivElement
+    cost: number = 0
+    progress: number = 0
 
     constructor(data: IProject) {
         this.name = data.name,
@@ -25,5 +31,43 @@ export class Project implements IProject {
         this.userRole = data.userRole,
         this.status = data.status,
         this.finishDate = data.finishDate
+        this.setUi ()
     }
+
+    setUi() {
+       if (this.ui) {return}
+       this.ui = document.createElement("div")
+       this.ui.className = "project-card"
+       this.ui.innerHTML = `
+            <div class="card-header">
+                <p style="background-color: #ca8134; padding: 10px; border-radius: 8px; aspect-ratio: 1; ">HC</p>
+                <div>
+                    <h5>${this.name}</h5>
+                    <p>${this.description}</p>
+                </div>
+            </div>
+            <div class="card-content">
+                <div class="card-property">
+                    <p style="color: #969696;">Status</p>
+                    <p>${this.status}</p>
+                </div>
+                <div class="card-property">
+                    <p style="color: #969696;">Status</p>
+                    <p>${this.userRole}</p>
+                </div>
+                <div class="card-property">
+                    <p style="color: #969696;">Status</p>
+                    <p>$${this.cost}</p>
+                </div>
+                <div class="card-property">
+                    <p style="color: #969696;">Status</p>
+                    <p>${this.progress * 100}%</p>
+                </div>
+            </div>
+                    `
+
+
+}
+
+
 }
